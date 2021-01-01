7f4c0ee2fbd205d074157dc942a154c263c7b470
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2021-01-01 23:03:07 +0100

    add header, main, footer in layout module

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    LayoutModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/client/src/app/layout/layout.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent, MainComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
+  ]
+})
+export class LayoutModule { }
