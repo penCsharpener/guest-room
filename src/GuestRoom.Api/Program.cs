0608b39a56a76f7fb37dd528d4d471cb0c971b6d
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2021-01-01 23:27:25 +0100

    update nugets, add serilog

--- src/GuestRoom.Api/Program.cs
+++ src/GuestRoom.Api/Program.cs
@@ -1,22 +1,21 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Serilog;
 
-namespace GuestRoom.Api {
-    public class Program {
-        public static void Main(string[] args) {
+namespace GuestRoom.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+                .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration));
+        }
     }
 }
