4f455266d4223dd7e55ddbac4248d704f4d16a3f
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2021-01-05 16:30:21 +0100

    draft layout of ng client

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './modules/account/login/login.component';
+import { RoomComponent } from './modules/main/room/room.component';
+import { TestComponent } from './modules/main/test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'account/login', component: LoginComponent },
+  { path: 'main/room/:id', component: RoomComponent },
+  { path: '', component: TestComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { TestComponent } from './modules/main/test/test.component';
+import { RoomComponent } from './modules/main/room/room.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TestComponent,
+    RoomComponent,
   ],
   imports: [
     BrowserModule,
