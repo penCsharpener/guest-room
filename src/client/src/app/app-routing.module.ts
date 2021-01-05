16b32ec29fb2d2d992795f2fffd206452853314d
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2021-01-05 22:53:20 +0100

    draft contact, legal, clean up draft

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './modules/account/login/login.component';
+import { ContactComponent } from './modules/main/contact/contact.component';
+import { LegalComponent } from './modules/main/legal/legal.component';
 import { RoomComponent } from './modules/main/room/room.component';
-import { TestComponent } from './modules/main/test/test.component';
+import { HomeComponent } from './modules/main/home/home.component';
 
 const routes: Routes = [
+  { path: 'main/contact', component: ContactComponent },
+  { path: 'main/legal', component: LegalComponent },
   { path: 'account/login', component: LoginComponent },
   { path: 'main/room/:id', component: RoomComponent },
-  { path: '', component: TestComponent },
+  { path: '', component: HomeComponent },
 ];
 
 @NgModule({
--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -4,14 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
-import { TestComponent } from './modules/main/test/test.component';
+import { HomeComponent } from './modules/main/home/home.component';
 import { RoomComponent } from './modules/main/room/room.component';
+import { LegalComponent } from './modules/main/legal/legal.component';
+import { ContactComponent } from './modules/main/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
+    HomeComponent,
     RoomComponent,
+    LegalComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
